e56e45ed7220bc67a451c86e3396da2849026f5d
xkHeitor <heitorcarvalhorod@gmail.com>
2022-05-17 10:19:41 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -17,7 +17,7 @@ const makeAddAccountRepository = (): AddAccountRepository => {
         id: 'valid_id', 
         name: 'valid_name',
         email: 'valid_email', 
-        password: 'valid_password'
+        password: 'hashed_password'
       }; 
       return new Promise(resolve => resolve(fakeAccount));
     }
@@ -99,6 +99,23 @@ describe('DbAddAccount', () => {
     
     const promise: Promise<AccountModel> = sut.add(accountData);
     await expect(promise).rejects.toThrow();
+  });
+
+  test('Should return an account on success', async () => { 
+    const { sut } = makeSut();
+    const accountData = {
+      name: 'valid_name', 
+      email: 'valid_email',
+      password: 'valid_password' 
+    };
+    
+    const account = await sut.add(accountData);
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name', 
+      email: 'valid_email',
+      password: 'hashed_password' 
+    });
   });
 
 });
