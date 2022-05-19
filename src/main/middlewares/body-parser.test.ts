0fc024a3a1605750315e00a47b9524c61e10fda8
xkHeitor <heitorcarvalhorod@gmail.com>
2022-05-19 10:32:11 -0300

    test: ensure request parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,14 @@
+import request from 'supertest'
+import app from '../config/app';
+
+describe('Body Parser Middleware', () => {
+
+  const route: string = '/test_body_parser';
+
+  test('Should parse body as json', async () => {
+    app.post(route, (req, res) => res.send(req.body));
+    const body = { name: 'any_name' };
+    await request(app).post(route).send(body).expect(body);
+  });
+
+});
