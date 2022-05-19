36c4a993ddaabd5f155a267f6eb4018a3298de01
xkHeitor <heitorcarvalhorod@gmail.com>
2022-05-19 10:31:45 -0300

    feat: ensure request parse body as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,4 @@
-import express, { Express } from 'express';
+import app from './config/app';
 
-const app: Express = express();
 const port: number = 5050;
-app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`)); 
