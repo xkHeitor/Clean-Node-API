2d98a7819d21a325e812f0981a7273eac8c1d9b1
xkHeitor <heitorcarvalhorod@gmail.com>
2022-05-19 11:27:26 -0300

    test: ensure request return json as default content type

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest'
+import app from '../config/app';
+
+describe('Content Type Middleware', () => {
+
+  test('Should return default content type as json', async () => {
+    const route: string = '/test_content_type';
+    app.get(route, (req, res) => res.send(''));
+    await request(app).get(route)
+      .expect('content-type', /json/);
+  });
+
+  test('Should return xml content type whe forced', async () => {
+    const route: string = '/test_content_type_xml';
+    app.get(route, (req, res) => {
+      res.type('.xml');
+      res.send('');
+    });
+    await request(app).get(route)
+      .expect('content-type', /xml/);
+  });
+
+});
