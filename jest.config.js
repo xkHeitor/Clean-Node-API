802b9e8dd123263cce1269317afd03bb54aae5ee
xkHeitor <heitorcarvalhorod@gmail.com>
2022-05-20 17:27:55 -0300

    chore: change coverage jest

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,9 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '<!rootDir>/src/main/**',
-    '<!rootDir>/src/**/index.ts'
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/**/*protocols.ts'
   ],
   coverageDirectory: 'coverage',
   preset: '@shelf/jest-mongodb',
