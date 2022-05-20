520f991548eb311b5ef6572cea947c65285de03a
xkHeitor <heitorcarvalhorod@gmail.com>
2022-05-20 10:26:55 -0300

    feat: connect to moongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,7 @@
-import app from './config/app'
+import { MongoHelper } from './../infra/db/mongodb/helpers/mongo-helper'
+import env from './config/env'
 
-const port: number = 5050
-app.listen(port, () => console.log(`Server running at http://localhost:${port}`)) 
+MongoHelper.connect(env.mongoURL).then(async () => {
+  const app = (await import('./config/app')).default
+  app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`)) 
+}).catch(console.error)
