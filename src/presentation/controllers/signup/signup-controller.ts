dd02b3f4e9e1adca7166d0bcbc10515ccae43e49
xkHeitor <heitorcarvalhorod@gmail.com>
2022-06-20 20:47:45 -0300

    feat: ensure SignUpController returns 403 if AddAccount returns null

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -1,5 +1,7 @@
+import { AccountModel } from './../../../domain/models/account'
+import { EmailInUseError } from './../../errors'
 import { Controller, HttpRequest, HttpResponse, AddAccount, Validation, Authentication } from './signup-controller-protocols'
-import { badRequest, serverError, ok } from '../../helpers/http/http-helper'
+import { badRequest, serverError, ok, forbidden } from '../../helpers/http/http-helper'
 
 export default class SignUpController implements Controller {
 
@@ -14,7 +16,8 @@ export default class SignUpController implements Controller {
     try {
       const error = this.validation.validate(httpRequest.body)
       if (error) return badRequest(error)
-      await this.addAccount.add({ name, email, password })
+      const account: AccountModel|null = await this.addAccount.add({ name, email, password })
+      if (!account) return forbidden(new EmailInUseError())
       const accessToken: string = await this.authentication.auth({ email, password })
       return ok({ accessToken })
     } catch (error: any) {
--- src/presentation/helpers/http/http-helper.ts
+++ src/presentation/helpers/http/http-helper.ts
@@ -1,18 +1,22 @@
 import { ServerError, UnauthorizedError } from '../../errors'
 import { HttpResponse } from '../../protocols'
 
-export const badRequest = (error: Error): HttpResponse => ({
-  statusCode: 400, body: error
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200, body: data
 })
 
-export const serverError = (error: Error): HttpResponse => ({
-  statusCode: 500, body: new ServerError(error?.stack)
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400, body: error
 })
 
 export const unauthorized = (): HttpResponse => ({
   statusCode: 401, body: new UnauthorizedError()
 })
 
-export const ok = (data: any): HttpResponse => ({
-  statusCode: 200, body: data
+export const forbidden = (error: Error): HttpResponse => ({
+  statusCode: 403, body: error
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500, body: new ServerError(error?.stack)
 })
