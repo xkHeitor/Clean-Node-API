c7eaf220775af7a337e5fda872d4e95a6b0f9bcb
xkHeitor <heitorcarvalhorod@gmail.com>
2022-06-30 16:02:56 -0300

    feat: ensure add survey route returns 204 if valid access token is provided

--- src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -1,4 +1,4 @@
-import { LoadAccountByTokenRepository } from './../../protocols/repository/account/load-account-by-token-repository';
+import { LoadAccountByTokenRepository } from './../../protocols/repository/account/load-account-by-token-repository'
 import { AccountModel } from '../add-account/db-add-account-protocols'
 import { LoadAccountByToken } from './../../../domain/usecases/load-account-by-token'
 import { Decrypter } from './../../protocols/criptograpy/decrypter'
@@ -10,7 +10,7 @@ export class DbLoadAccountByToken implements LoadAccountByToken {
   async load (accessToken: string, role?: string): Promise<AccountModel|null> {
     const token: string = await this.decrypter.decrypt(accessToken)
     if (token) {
-      const account: AccountModel = await this.loadAccountByTokenRepository.loadByToken(token, role)
+      const account: AccountModel = await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
       return account||null
     }
     return null
