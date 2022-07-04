63b7b95580b92c49dddad6bbcfee6e41e848e02f
xkHeitor <heitorcarvalhorod@gmail.com>
2022-07-04 10:58:24 -0300

    chore: edit env mongo test

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 export default {
   mongoURL: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api',
-  mongoTest: global.__MONGO_URI__,
+  mongoTest: global.__MONGO_URI__ ?? 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT ?? 5050,
   jwtSecret: process.env.JWT_SECRET ?? 'cNa/13-GGbh1@27017'
 }
