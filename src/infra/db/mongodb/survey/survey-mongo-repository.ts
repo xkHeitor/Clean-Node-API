df994df8e7d6c2bd41391fe3d509dbbe583ecb8e
xkHeitor <heitorcarvalhorod@gmail.com>
2022-07-25 20:48:13 -0300

    feat: ensure SurveyMongoRepository find survey by id with success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,22 +1,31 @@
+import { LoadSurveyByIdRepository } from '@/data/protocols/repository/survey/load-survey-by-id-repository'
 import { AddSurveyRepository } from '@/data/protocols/repository/survey/add-survey-repository'
 import { LoadSurveysRepository } from '@/data/protocols/repository/survey/load-surveys-repository'
 import { AddSurveyModel } from '@/domain/usecases/add-survey'
 import { SurveyModel } from '@/domain/models/survey'
 import { MongoHelper } from './../helpers/mongo'
-import { Collection, Document } from 'mongodb'
+import { Collection, Document, ObjectId } from 'mongodb'
 
-export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
-  
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyByIdRepository {
+
+  private readonly collectionName: string = 'surveys'
+
   async add(surveyData: AddSurveyModel): Promise<void> {
-    const surveyCollection: Collection<Document> = await MongoHelper.getCollection('surveys')
+    const surveyCollection: Collection<Document> = await MongoHelper.getCollection(this.collectionName)
     await surveyCollection.insertOne(surveyData)
   }
   
   async loadAll(): Promise<SurveyModel[]> {
-    const surveyCollection: Collection<Document> = await MongoHelper.getCollection('surveys')
+    const surveyCollection: Collection<Document> = await MongoHelper.getCollection(this.collectionName)
     const surveys = await surveyCollection.find().toArray()
-    const test = MongoHelper.mapAll(surveys)
-    return test
+    const surveyModel = MongoHelper.mapAll(surveys)
+    return surveyModel
+  }
+
+  async loadById(id: string): Promise<SurveyModel> {
+    const surveyCollection: Collection<Document> = await MongoHelper.getCollection(this.collectionName)
+    const survey: ReturnType<any> = await surveyCollection.findOne({ _id: new ObjectId(id) })
+    return survey
   }
 
 }
