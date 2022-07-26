ddcd36af5f56e96326c10695120886970d971201
xkHeitor <heitorcarvalhorod@gmail.com>
2022-07-26 19:18:23 -0300

    ffeat: ensure SurveyResultMongoRepository insert new  SurveyResult if its new

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,28 @@
+import { SaveSurveyResultModel, SaveSurveyResultRepository } from '@/data/usecases/save-survey-result/db-save-survey-result-protocols'
+import { SurveyResultModel } from '@/domain/models/survey-result'
+import { MongoHelper } from '../helpers/mongo'
+import { Collection } from 'mongodb'
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  
+  private readonly collectionName: string = 'surveys'
+
+  async save(data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyCollection: Collection = await MongoHelper.getCollection(this.collectionName)
+    const survey: ReturnType<any> = await surveyCollection.findOneAndUpdate({
+      surveyId: data.surveyId,
+      accountId: data.accountId
+    }, {
+      $set: {
+        answer: data.answer,
+        date: data.date
+      }
+    }, {
+      upsert: true,
+      returnDocument: 'after'
+    })
+
+    return survey.value && MongoHelper.map(survey.value) 
+  }
+
+}
